d12b05e8ef4723193b83014c83bd5736c26c9830
webdiegooviedo <84927667+webdiegooviedo@users.noreply.github.com>
2021-06-20 18:01:59 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -7,3 +7,31 @@ firebase.initializeApp({
     appId: "1:685889139313:web:96df646e3c3f797d78e1d0",
     measurementId: "G-0HQBLHD2QS"
 });
+
+
+
+const txtEmail = document.getElementById('txtEmail')
+   const txtPassword = document.getElementById('txtPassword')
+   const btnLogin = document.getElementById('btnLogin')
+   const btnSignUp = document.getElementById('btnSignUp')
+   const btnLogout = document.getElementById('btnLogout')
+
+   btnLogin.addEventListenner('click', e => {
+   	const email = txtEmail.value;
+   	const pass = txtPassword.value;
+   	const auth = firebase.auth();
+
+   	const promise = auth.signInWithEmailAndPassword(email, pass);
+   	promise.catch(e => console.log(e.message));
+   });
+
+   btnSignUp.addEventListenner('click', e => {
+   	const email = txtEmail.value;
+   	const pass = txtPassword.value;
+   	const auth = firebase.auth();
+
+   	const promise = auth.createUserWithEmailAndPassword(email, pass);
+   	promise.catch(e => console.log(e.message));
+   });
+
+}()); 
