193e82eb7eec4f7bfdd6361b6143b711659f3e05
webdiegooviedo <84927667+webdiegooviedo@users.noreply.github.com>
2021-06-20 16:34:43 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyCRqX4GmBVGbGe2w9CU2GCZT1LgDSmIKR4",
+    authDomain: "fbwebauth10.firebaseapp.com",
+    projectId: "fbwebauth10",
+    storageBucket: "fbwebauth10.appspot.com",
+    messagingSenderId: "685889139313",
+    appId: "1:685889139313:web:96df646e3c3f797d78e1d0",
+    measurementId: "G-0HQBLHD2QS"
+});
